171636324755844145702e8fa65542d71caccb8c
Lz-Rod <lz.rod@outlook.com>
2023-08-24 00:29:15 -0300

    Update - Sistema CRUD básico finalizado

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cliente
 
-# Create your views here.
+def home(request):
+    vcliente = Cliente.objects.all()
+    return render(request, "index.html", {"vcliente": vcliente})
+
+def save(request):
+    vnome = request.POST.get("nome")
+    Cliente.objects.create(nome=vnome)
+    vcliente = Cliente.objects.all()
+    return render(request, "index.html", {"vcliente": vcliente})
+
+def edit(request, id):
+    vcliente = Cliente.objects.get(id=id)
+    return render(request, "update.html", {"vcliente": vcliente})
+
+def update(request, id):
+    vnome = request.POST.get("nome")
+    vcliente = Cliente.objects.get(id=id)
+    vcliente.nome = vnome
+    vcliente.save()
+    return redirect(home)
+
+def delete(request, id):
+    vcliente = Cliente.objects.get(id=id)
+    vcliente.delete()
+    return redirect(home)
